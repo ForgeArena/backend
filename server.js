38e525eb5f1e28ad97ffa5eb05add395508ad700
Juhasz Antal <skillforgearena@gmail.com>
2025-02-17 21:42:54 +0200

    Update BD

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../config/.env" });
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
@@ -16,5 +16,5 @@ connectDB();
 // Routes
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
